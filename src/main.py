99475ffbb03d6a46e8033aada8ff0b93c8cba54b
Martin <mcabrera.ezequiel@gmail.com>
2025-04-06 04:39:58 +0000

    Testing conexion a la database

--- src/main.py
+++ src/main.py
@@ -1,11 +1,39 @@
 from pymongo import MongoClient
+from faker import Faker
+import json
 import os
 
 mongo_uri = os.environ.get("MONGO_URI")
 db_name = os.environ.get("DB_NAME")
+collection_name = "testeo"
 
-client = MongoClient(mongo_uri)
-db = client[db_name]
+fake = Faker('es_ES')
+destinos = []
 
-print("Colecciones disponibles:")
-print(db.list_collection_names())
+for i in range(15):
+    destino = {
+        "nombre": fake.city(),
+        "pais": fake.country(),
+        "clima": fake.random_element(["Tropical", "Templado", "Árido", "Mediterráneo", "Continental", "Polar", "Desértico", "Seco"]),
+        "actividades": fake.words(nb=3, unique=True),
+        "costo_promedio": fake.random_int(500, 5000),
+        "puntuacion": fake.random_int(1, 5)
+    }
+    destinos.append(destino)
+
+with open("data/destinos.json", "w", encoding="utf-8") as f:
+    json.dump(destinos, f, ensure_ascii=False, indent=4)
+
+try:
+    client = MongoClient(mongo_uri)
+    db = client[db_name]
+    collection = db[collection_name]
+
+    result = collection.insert_many(destinos)
+    print(f"Se insertaron {len(result.inserted_ids)} documentos en esta colección")
+except Exception as e:
+    print(f"Error al insertar documentos: {e}")
+finally:
+    client.close()
+    print("Conexión cerrada")
+
