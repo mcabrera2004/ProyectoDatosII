ac427ac5148c33438dd4544831a0f50a6f41a37e
Martin <mcabrera.ezequiel@gmail.com>
2025-04-07 23:11:35 +0000

    HTML con imagenes

--- src/api.py
+++ src/api.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 import os
 
 
-app = Flask(__name__, template_folder="../templates")
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
 # Configuración de MongoDB
 MONGO_URI = os.environ.get("MONGO_URI")
